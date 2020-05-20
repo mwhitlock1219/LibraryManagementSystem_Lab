6d3a4b1011894d849169f3ec01448bc36604188a
Melina Alejos <malejos1219@gmail.com>
2020-05-20 14:12:55 -0400

    Add 'ReferenceBook.java' and 'GeneralBook.java' classes as children of 'Book.java' class

---
+++ GeneralBook.java
@@ -0,0 +1,7 @@
+public class GeneralBook extends Book {
+    public GeneralBook(String author, String title, String genre, String indexLocation, int yearReleased,
+            int numOfPages) {
+        super(author, title, genre, indexLocation, yearReleased, numOfPages);
+
+    }
+}
---
+++ ReferenceBook.java
@@ -0,0 +1,7 @@
+public class ReferenceBook extends Book {
+    public ReferenceBook(String author, String title, String genre, String indexLocation, int yearReleased,
+            int numOfPages) {
+        super(author, title, genre, indexLocation, yearReleased, numOfPages);
+
+    }
+}
