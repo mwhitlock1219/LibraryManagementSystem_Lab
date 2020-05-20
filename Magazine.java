0d7b68e58cbed50d42be9c447a6828b5a3e17f31
Melina Alejos <malejos1219@gmail.com>
2020-05-20 14:15:02 -0400

    Add 'Magazine.java' and 'NewsPaper.java' classes as children of 'Periodical.java' class

---
+++ Magazine.java
@@ -0,0 +1,5 @@
+public class Magazine extends Periodical {
+    public Magazine(String author, String title, String genre, String indexLocation, int yearReleased) {
+        super(author, title, genre, indexLocation, yearReleased);
+    }
+}
---
+++ NewsPaper.java
@@ -0,0 +1,5 @@
+public class NewsPaper extends Periodical {
+    public NewsPaper(String author, String title, String genre, String indexLocation, int yearReleased) {
+        super(author, title, genre, indexLocation, yearReleased);
+    }
+}
